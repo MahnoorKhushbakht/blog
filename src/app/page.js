a8f932a5f2a6f1eaf6476ebca48cdc8528a08b5d
Mahnoor Khushbakht <mahnoorkhushbakht@gmail.com>
2024-10-25 13:26:02 -0700

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
             <Background/>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
   
-        <h1 className="md:text-6xl text-3xl justify-items-center text-red-100 antialiased font-bold ml-10 mr-10">Echoes of Thought</h1>
+        <h1 className="md:text-6xl text-3xl justify-items-center text-red-100 antialiased font-bold md:ml-10 ml-5 mr-5 md:mr-10">Echoes of Thought</h1>
         <h1 className="md:text-2xl underline underline-offset-2 hover:decoration-4 justify-items-center text-red-100 antialiased md:ml-10 ml-5 mr-5 md:mr-10 text-xl">It is a space for curious minds and thoughtful souls. We explore a range of topics, from personal reflections and lifestyle tips to travel stories and insightful observations. Join us as we share ideas, inspire conversations, and celebrate the beauty of everyday life.</h1>
       <CardInfo/>
       </main>
