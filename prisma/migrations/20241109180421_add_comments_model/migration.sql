633b3aff1c18027ba67ef2f68a6278d7ae6ac0f5
Mahnoor Khushbakht <mahnoorkhushbakht@gmail.com>
2024-11-09 10:05:58 -0800

    updated

---
+++ prisma/migrations/20241109180421_add_comments_model/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "slug" TEXT NOT NULL,
+    "user" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "postedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
