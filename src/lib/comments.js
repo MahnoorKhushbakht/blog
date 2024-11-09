7a2e5a11bda644144560e6a53566d41390c441e5
Mahnoor Khushbakht <mahnoorkhushbakht@gmail.com>
2024-11-09 11:50:31 -0800

    updated

--- src/lib/comments.js
+++ src/lib/comments.js
@@ -7,9 +7,17 @@ export async function createComment({ slug, user, message,type }) {
   }
 
   
-export async function getComments(slug) {
-  return await db.comment.findMany({
-    where: { slug },
-    orderBy: { postedAt: 'desc' },
-  });
-}
+
+  export async function getComments(slug) {
+    try {
+      const comments = await db.comment.findMany({
+        where: { slug },
+        orderBy: { postedAt: 'desc' },
+      });
+      return comments || [];  
+    } catch (error) {
+      console.error("Error fetching comments:", error);
+      return [];  
+    }
+  }
+  
