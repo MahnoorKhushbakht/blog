86ec0891641f60c34ae732e18483535d9c0783cf
Mahnoor Khushbakht <mahnoorkhushbakht@gmail.com>
2024-11-11 12:15:46 -0800

    updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function App() {
   return (
     <header className="text-gray-600 body-font bg-red-300">
-      <div className="container mx-auto flex justify-center items-center p-5">
+      <div className="container mx-auto flex justify-between items-center p-5">
         <a className="flex title-font font-medium items-center" href="/">
           <Image
             src="/images/eot-logo.png"  
@@ -13,7 +13,13 @@ export default function App() {
             height={40} 
             className="rounded-full bg-white"
           />
-          <span className="ml-3 text-xl text-white">Echoes of Thought</span>
+        <span className="ml-3 text-xl hidden md:block text-white">Echoes of Thought</span>
+        </a>
+        <a 
+          href="/sign-in" 
+          className="text-red-500 bg-red-100 hover:bg-red-500 hover:text-red-100 py-2 px-4 rounded-md text-lg"
+        >
+          Sign In
         </a>
       </div>
     </header>
