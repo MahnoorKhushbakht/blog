0634961b25c09597a1b718c1782e23b982958d68
Mahnoor Khushbakht <mahnoorkhushbakht@gmail.com>
2024-11-19 20:33:26 +0500

    updated

--- src/components/googleSignIn/config.js
+++ src/components/googleSignIn/config.js
@@ -2,14 +2,15 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain ,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket ,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
+
 
 // Check if Firebase is already initialized
 let app;
