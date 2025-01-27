6f99d3852d4bb885c33df709b7f31b9194d7a3e4
Emmanuel-bot-rgb <pe699674@gmail.com>
2025-01-28 01:42:12 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cms-strapi-sm20.onrender.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cms-strapi-sm20.onrender.com',
+        pathname: '/uploads/**', // Allow images under the uploads folder
+      },
+    ],
   },
 };
 
 export default nextConfig;
+
+// https://cms-strapi-sm20.onrender.com
